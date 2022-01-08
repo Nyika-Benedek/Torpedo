7689ff52b1ace85d5e93a431f51aaf9e8987c2a8
Nyika-Benedek <fiokbluestacks@gmail.com>
2022-01-08 08:38:51 +0100

    MyVector added to define ships positioning

---
+++ Torpedo/Models/MyVector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Torpedo.Models
+{
+    // The name Vector will mess up the inner functioning of the WPF
+
+    public enum Direction { Horizontal, Vertical };
+    class MyVector
+    {
+        public Direction Way { get; private set; }
+        public int Size { get; private set; }
+
+        public MyVector(Direction way, int size)
+        {
+            Way = way;
+            Size = size;
+        }
+    }
+}
