422ca6a9ad79f2805730560a2ae3e31a211a374d
Kiss Kristóf <kristofkiss@mailbox.unideb.hu>
2022-01-09 14:04:39 +0100

    Add AI integration template

---
+++ Torpedo/Models/AI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Torpedo.Models
+{
+    public class AI : Player
+    {
+        private static readonly string _aiName = "AI";
+        public AI() : base(_aiName)
+        {
+        }
+        public Coordinate Act(IBattlefield enemyBattlefield)
+        {
+            return new Coordinate(0, 0); // TODO create an agent that decides where to shoot
+        }
+    }
+}
