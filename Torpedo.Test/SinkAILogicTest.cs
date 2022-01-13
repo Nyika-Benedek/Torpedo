806dd5f85c8c7f8622bac5c74422068c5fa223e1
SapiGabor <gaborsapi9999@gmail.com>
2022-01-13 22:48:15 +0100

    SinkAILogic test started.

---
+++ Torpedo.Test/SinkAILogicTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Torpedo;
+using Torpedo.AIModule;
+using Torpedo.Interfaces;
+using Torpedo.Models;
+
+namespace Torpedo.Test
+{
+    public class SinkAILogicTest
+    {
+        [TestMethod]
+        public void aha()
+        {
+            List<IShips> ships = new List<IShips>();
+            Battlefield battlefield = new Battlefield(ships);
+            Coordinate x = new Coordinate(5, 7);
+            Coordinate y = new Coordinate(5, 8);
+            SinkAILogic logic = new SinkAILogic(battlefield, x, y);
+            logic.proposed();
+        }
+
+    }
+}
