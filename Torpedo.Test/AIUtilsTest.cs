821fa3e1c910945759b1a162d405f5a2a23df599
Kiss Kristóf <kristofkiss@mailbox.unideb.hu>
2022-01-14 00:01:58 +0100

    comment for tests

--- Torpedo.Test/AIUtilsTest.cs
+++ Torpedo.Test/AIUtilsTest.cs
@@ -1,51 +1,63 @@
 namespace TestProject3
 {
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
     using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Torpedo;
     using Torpedo.Interfaces;
     using Torpedo.Models;
 
-#pragma warning disable NI1007 // Test classes must ultimately inherit from 'AutoTest'
+    /// <summary>
+    /// Tests the <see cref="AIUtils"/>.
+    /// </summary>
     [TestClass]
-    public class UnitTest1
+    public class AIUtilsTest
     {
+        /// <summary>
+        /// Tests IsInField with <see cref="Coordinate"/> in field.
+        /// </summary>
         [TestMethod]
-        public void IsInField_ReturnTrue()
+        public void IsInField_InField_True()
         {
             Coordinate x = new Coordinate(4, 8);
-            // Arrange + Act + Assert
             Assert.IsTrue(AIUtils.IsInField(x));
         }
 
+        /// <summary>
+        /// Tests IsInField with Y <see cref="Coordinate"/> out of Field.
+        /// </summary>
         [TestMethod]
-        public void IsInField_y_ReturnFalse()
+        public void IsInField_YOutside_False()
         {
             Coordinate x = new Coordinate(0, 12);
-            // Arrange + Act + Assert
             Assert.IsFalse(AIUtils.IsInField(x));
         }
 
+        /// <summary>
+        /// Tests IsInField with X <see cref="Coordinate"/> out of Field.
+        /// </summary>
         [TestMethod]
-        public void IsInField_x_ReturnFalse()
+        public void IsInField_XOutside_False()
         {
             Coordinate x = new Coordinate(12, 0);
-            // Arrange + Act + Assert
             Assert.IsFalse(AIUtils.IsInField(x));
         }
 
+        /// <summary>
+        /// Tests IsInField with both x and y <see cref="Coordinate"/> out of Field.
+        /// </summary>
         [TestMethod]
-        public void IsInField_xy_ReturnFalse()
+        public void IsInField_BothOutside_False()
         {
             Coordinate x = new Coordinate(11, 12);
-            // Arrange + Act + Assert
             Assert.IsFalse(AIUtils.IsInField(x));
         }
 
+        /// <summary>
+        /// Tests IsInField with absurd <see cref="Coordinate"/> inputs.
+        /// </summary>
         [TestMethod]
-        public void IsInField_edgecases_ReturnFalse()
+        public void IsInField_EdgeCases_False()
         {
-            // Arrange + Act + Assert
             Assert.IsFalse(AIUtils.IsInField(new Coordinate(int.MinValue, int.MinValue)));
             Assert.IsFalse(AIUtils.IsInField(new Coordinate(int.MaxValue, int.MaxValue)));
             Assert.IsFalse(AIUtils.IsInField(new Coordinate(int.MinValue, 2)));
@@ -56,42 +68,63 @@ namespace TestProject3
             Assert.IsFalse(AIUtils.IsInField(new Coordinate(int.MaxValue, int.MinValue)));
         }
 
+        /// <summary>
+        /// Tests IsCellShot with an already shot <see cref="Coordinate"/>, hitting a ship.
+        /// </summary>
         [TestMethod]
-        public void IsCellShot_ReturnTrue()
+        public void IsCellShot_Hit_True()
         {
+            // Arrange
             MyVector vector = new MyVector(IShips.Direction.Horizontal, 5);
             Ship ship = new Ship(new Coordinate(0, 0), vector);
             List<IShips> list = new List<IShips>();
             list.Add(ship);
             Battlefield battlefield = new Battlefield(list);
             Coordinate x = new Coordinate(0, 0);
+
+            // Act
             battlefield.Shoot(x);
-            // Arrange + Act + Assert
+
+            // Assert
             Assert.IsTrue(AIUtils.IsCellShot(battlefield, x));
         }
 
+        /// <summary>
+        /// Tests IsCellShot with an already shot <see cref="Coordinate"/>, missing a ship.
+        /// </summary>
         [TestMethod]
-        public void IsCellShot_MissShip_ReturnTrue()
+        public void IsCellShot_Miss_True()
         {
+            // Arrange
             MyVector vector = new MyVector(IShips.Direction.Horizontal, 5);
             Ship ship = new Ship(new Coordinate(0, 0), vector);
             List<IShips> list = new List<IShips>();
             list.Add(ship);
             Battlefield battlefield = new Battlefield(list);
             Coordinate x = new Coordinate(0, 4);
+
+            // Act
             battlefield.Shoot(x);
-            // Arrange + Act + Assert
+
+            // Assert
             Assert.IsTrue(AIUtils.IsCellShot(battlefield, x));
         }
 
+        /// <summary>
+        /// Tests IsCellShot with an empty <see cref="Battlefield"/>.
+        /// </summary>
         [TestMethod]
-        public void IsCellShot_WithoutShip_ReturnFalse()
+        public void IsCellShot_EmptyBattlefield_False()
         {
+            // Arrange
             List<IShips> list = new List<IShips>();
             Battlefield battlefield = new Battlefield(list);
             Coordinate x = new Coordinate(0, 0);
+
+            // Act
             battlefield.Shoot(x);
-            // Arrange + Act + Assert
+
+            // Assert
             Coordinate y = new Coordinate(1, 0);
             Assert.IsFalse(AIUtils.IsCellShot(battlefield, y));
         }
