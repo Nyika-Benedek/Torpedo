7c55d5eea4927860d3fd5d594c3437dda6523aa2
Nyika-Benedek <fiokbluestacks@gmail.com>
2022-01-14 00:08:39 +0100

    adding AAA

--- Torpedo.Test/FoundAILogicTest.cs
+++ Torpedo.Test/FoundAILogicTest.cs
@@ -39,13 +39,18 @@
         [TestMethod]
         public void Plan_OneSot_MissingShotAndFocus()
         {
+            // Arrange
             List<IShips> ships = new List<IShips>();
             Battlefield battlefield = new Battlefield(ships);
             Coordinate x = new Coordinate(0, 1);
+
+            // Act
             battlefield.Shoot(x);
             Coordinate focus = new Coordinate(1, 1);
             FoundAILogic logic = new FoundAILogic(battlefield, focus);
             List<Coordinate> actual = logic.Plan();
+
+            // Assert
             Assert.IsFalse(actual.Contains(x));
             Assert.IsFalse(actual.Contains(focus));
         }
@@ -56,14 +61,19 @@
         [TestMethod]
         public void Plan_WithShot_MissingCoordinates()
         {
+            // Arrange
             List<IShips> ships = new List<IShips>();
             Battlefield battlefield = new Battlefield(ships);
             Coordinate x = new Coordinate(1, 1);
             Coordinate y = new Coordinate(-1, 1);
+
+            // Act
             battlefield.Shoot(x);
             Coordinate focus = new Coordinate(0, 1);
             FoundAILogic logic = new FoundAILogic(battlefield, focus);
             List<Coordinate> actual = logic.Plan();
+
+            // Assert
             Assert.IsFalse(actual.Contains(x));
             Assert.IsFalse(actual.Contains(y));
             Assert.IsFalse(actual.Contains(focus));
@@ -75,9 +85,12 @@
         [TestMethod]
         public void Constructor_OutOfBattleField_ThrowsError()
         {
+            // Arrange
             List<IShips> ships = new List<IShips>();
             Battlefield battlefield = new Battlefield(ships);
             Coordinate focus = new Coordinate(-1, 0);
+
+            // Act + Assert
             Assert.ThrowsException<ArgumentException>(() => new FoundAILogic(battlefield, focus));
         }
     }
